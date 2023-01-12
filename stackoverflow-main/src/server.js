5b900501f92258f41aea6e2f493f4bab3906aade
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2023-01-12 22:01:54 +0530

    started working

--- stackoverflow-main/src/server.js
+++ stackoverflow-main/src/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const cors = require('cors');
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Stackoverflow");
+})
+console.log(process.env.PORT,"env")
 const PORT = process.env.PORT || 8080; // port at which server listening
 
 app.listen(
